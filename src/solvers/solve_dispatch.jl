ada47d14aa0d57d04d3b4f2dd4a4afff1fd43149
SE Choi <seechoi@uni-osnabrueck.de>
2026-05-20 13:45:35 +0200

    refactor cpd script to julia dispatch structure

--- src/solvers/solve_dispatch.jl
+++ src/solvers/solve_dispatch.jl
@@ -45,6 +45,10 @@ function _solver_object(::Val{:btd_tsd}, stepsize::Real; kwargs...)
     )
 end
 
+function _solver_object(::Val{:tsd}, stepsize::Real; kwargs...)
+    return _solver_object(Val(:btd_tsd), stepsize; kwargs...)
+end
+
 function _solver_object(::Val{S}, ::Real; kwargs...) where {S}
     throw(
         ArgumentError(
